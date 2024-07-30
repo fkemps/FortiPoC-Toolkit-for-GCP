ddf4189d687ee342870f69c127907be91596b93a
Ferry Kemps <fkemps@4xion.com>
2024-07-30 22:41:18 +0200

    Major update to support multi-project, increased performance

--- gcpcmd.sh
+++ gcpcmd.sh
@@ -625,8 +625,9 @@ function displayhelp {
    echo '| |_ / _ \|  __| __| | |_) / _ \ / __|   | |/ _ \ / _ \| | |/ / | __| | |_ / _ \|  __| | |  _| |   | |_) |'
    echo '|  _| (_) | |  | |_| |  __/ (_) | (__    | | (_) | (_) | |   <| | |_  |  _| (_) | |    | |_| | |___|  __/'
    echo '|_|  \___/|_|   \__|_|_|   \___/ \___|   |_|\___/ \___/|_|_|\_\_|\__| |_|  \___/|_|     \____|\____|_|'
-   echo ""
    echo "(Version: ${GCPCMDVERSION})"
+   echo ""
+   echo "Selected project : ${GCPPROJECT}"
    echo "Default deployment region: ${ZONE}"
    echo "Personal instance identification: ${FPPREPEND}"
    echo "Default product: ${PRODUCT}"
